272a3e1840f1650e0aab557f73bf0b7d1674cdb6
athmenfartas <athmenfartas@gmail.com>
2026-05-16 14:45:01 +0100

    chore: remove old unused components

---
+++ src/components/ui/data-table/index.ts
@@ -0,0 +1,5 @@
+export { DataTable } from './data-table'
+export { DataTablePagination } from './data-table-pagination'
+export { DataTableToolbar } from './data-table-toolbar'
+export { DataTableFacetedFilter } from './data-table-faceted-filter'
+export { DataTableViewOptions } from './data-table-view-options'
