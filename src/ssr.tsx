3cfb7c9489e8cf3ffc09d8ebe89b84c3300ae754
axonio-solutions <axonio.team@gmail.com>
2026-06-03 10:26:55 +0100

    fix: catch AbortError in SSR stream handler from client disconnects

--- src/ssr.tsx
+++ src/ssr.tsx
@@ -6,6 +6,19 @@ import {
 
 import { createRouter } from './router'
 
+const safeHandler = (
+  opts: Parameters<typeof defaultStreamHandler>[0],
+) => {
+  try {
+    return defaultStreamHandler(opts)
+  } catch (err) {
+    if (err instanceof DOMException && err.name === 'AbortError') {
+      return new Response(null, { status: 499 })
+    }
+    throw err
+  }
+}
+
 export default (createStartHandler as any)({
   getRouterManifest,
-})((defaultStreamHandler as any)(createRouter))
+})((safeHandler as any)(createRouter))
